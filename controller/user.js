0bc231fbdaa77f74ec8197bae25f311a8a8cb46c
Pradhan121 <sm.pradhan101@gmail.com>
2026-02-26 21:38:52 +0530

    new code added

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,5 @@
 let USER = require('../model/user')
+let dashboardUser = require('../model/userDashboard')
 
 exports.homePage = async(req,res)=>{
     const data = await USER.find()
@@ -12,28 +13,50 @@ exports.registerToLoginPage = (req,res)=>{
 }
 exports.createAccount = async(req,res)=>{
     const data = req.query
-    await USER.insertOne(data)
+    await USER.create(data)
     res.redirect('/')
 }
 exports.loginUser = async(req,res)=>{
     const data = req.query
     const user = await USER.findOne(data)
-     const studentData = await USER.find()
+     const studentData = await dashboardUser.find()
     if(user){
-        res.render('Dashboard.ejs',{studentData})
+        res.render('Dashboard.ejs',{studentData,result:null})
     }
     else{
         res.send('Invalid email or password')
     }
 }
 exports.dashboardData =async (req, res) => {
-  const data = req.query;
-  await USER.insertOne(data);
-  console.log(data);
+  const data = req.query
 
-  res.redirect("/dashboard");
+  if (data.id !== "") {
+      await dashboardUser.findByIdAndUpdate(data.id, {
+        userName: data.userName,
+        age: data.age,
+        studentClass:data.studentClass,
+        address:data.address
+      })
+  } 
+  else {
+    await dashboardUser.create(data)
+  }   
+  res.redirect('/dashboard')
 };
 exports.dashboard = async (req, res) => {
-  const studentData = await USER.find()
-  res.render("Dashboard.ejs", { studentData });
-};
+    const studentData = await dashboardUser.find()
+    res.render("Dashboard.ejs", { studentData, result: null })
+}
+exports.deleteData = async(req,res)=>{
+   const deleteId = req.params.id
+   await dashboardUser.findByIdAndDelete(deleteId)
+   res.redirect('/')
+}
+exports.editData = async (req, res) => {
+  const editId = req.params.id
+
+  const result = await dashboardUser.findById(editId)
+  const studentData = await dashboardUser.find()
+
+  res.render("Dashboard.ejs", { studentData, result })
+}
